5a57aacb36770d33b058c86c753f100e8b24575a
tushx3 <tkushwaha70@gmail.com>
2021-02-25 11:19:30 +0530

    Initial commit
    Generated by create-react-native-app 3.5.3.

---
+++ App.js
@@ -0,0 +1,26 @@
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  const [fontsLoaded] = Font.useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+    'Inter-SemiBoldItalic':
+      'https://rsms.me/inter/font-files/Inter-SemiBoldItalic.otf?v=3.12',
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Platform Default</Text>
+      <Text style={{ fontFamily: "Inter-Black" }}>Inter Black</Text>
+      <Text style={{ fontFamily: "Inter-SemiBoldItalic" }}>
+        Inter SemiBoldItalic
+      </Text>
+    </View>
+  );
+}
